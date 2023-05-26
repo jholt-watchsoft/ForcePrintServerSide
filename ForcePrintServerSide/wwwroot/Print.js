612863dc97c9359e4928624ea21317da2b396124
jholt <jholt@ASUS-DUO-I9>
2023-05-26 17:33:06 +0100

    Add project files.

---
+++ ForcePrintServerSide/wwwroot/Print.js
@@ -0,0 +1,20 @@
+﻿function printLabel() {
+    const labelContainer = document.getElementById('labelContainer');
+    const printWindow = window.open('', '', 'width=600,height=600');
+    printWindow.document.write('<html><head><title>Print</title></head><body>');
+    printWindow.document.write(labelContainer.innerHTML);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+
+    printWindow.addEventListener('load', () => {
+        printWindow.focus();
+        printWindow.print();
+        printWindow.close();
+    });
+}
+
+export { printLabel };
+
+window.printInterop = {
+    printLabel: printLabel
+};
